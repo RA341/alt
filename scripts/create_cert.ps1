b15440c39b651f6b51172622c9a9cebc60cebc02
RA341 <razerishi1505@gmail.com>
2024-08-17 23:17:42 -0400

    ci

--- scripts/create_cert.ps1
+++ scripts/create_cert.ps1
@@ -6,24 +6,21 @@ param (
 # Convert password to SecureString
 $SecurePassword = ConvertTo-SecureString -String $Password -Force -AsPlainText
 
-# Generate the self-signed certificate
+$cert = New-SelfSignedCertificate `
+    -CertStoreLocation Cert:\CurrentUser\My `
+    -DnsName $Domain `
+    -FriendlyName "My Self-Signed Certificate" `
 
-$cert = New-SelfSignedCertificate -Type Custom -Subject "alt self signed" -KeyUsage DigitalSignature -FriendlyName "ALT self signed" -CertStoreLocation "Cert:\CurrentUser\My" -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.3", "2.5.29.19={text}")
-# Check if certificate was created successfully
-if ($null -eq $cert) {
-    Write-Host "Certificate creation failed."
-    exit 1
-}
 
 # Verify the certificate store path
-$certStorePath = "Cert:\LocalMachine\My\$($cert.Thumbprint)"
+$certStorePath = "Cert:\CurrentUser\My\$($cert.Thumbprint)"
 if (-not (Test-Path $certStorePath)) {
     Write-Host "Certificate path does not exist: $certStorePath"
     exit 1
 }
 
 # Define the path where the certificate will be saved
-$certPath = "$env:USERPROFILE\Desktop\mycert.pfx"
+$certPath = "mycert.pfx"
 
 # Export the certificate to a PFX file
 try {
